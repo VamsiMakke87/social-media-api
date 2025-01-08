bd49879b06a5e5e4eeb776583fb26f3532c482fb
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-01-08 16:18:26 +0530

    Added notification for add comment and like comment

--- routes/comment.js
+++ routes/comment.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const Post = require("../models/post");
 const User = require("../models/user");
 const Comment = require("../models/comment");
+const Notification = require("../models/notifications");
 const Reply = require("../models/replies");
 
 // Create a comment
@@ -13,10 +14,24 @@ router.post("/", async (req, res) => {
     const data = await newComment.save();
     // console.log(data);
     await post.updateOne({ $push: { comments: data._doc._id } });
+    if (req.body.userId != post.userId) {
+      const notificationData = {
+        fromUserId: req.body.userId,
+        toUserId: post.userId,
+        postId: post._id,
+        description: newComment.description,
+        type: 2,
+      };
+      const newNotification = new Notification(notificationData);
+      await newNotification.save();
+      await User.findByIdAndUpdate(post.userId, {
+        $set: { hasUnreadNotifications: true },
+      });
+    }
 
     res.status(200).json(data);
   } catch (err) {
-    // console.log(err);
+    console.log(err);
     res.status(500).json(err);
   }
 });
@@ -65,12 +80,27 @@ router.put("/like/:id", async (req, res) => {
       const data = await comment.updateOne({
         $push: { likes: req.body.userId },
       });
+      if (req.body.userId != comment.userId) {
+        const notificationData = {
+          fromUserId: req.body.userId,
+          toUserId: comment.userId,
+          postId: comment.postId,
+          description: comment.description,
+          type: 3,
+        };
+        const newNotification = new Notification(notificationData);
+        await newNotification.save();
+        await User.findByIdAndUpdate(comment.postUserId, {
+          $set: { hasUnreadNotifications: true },
+        });
+      }
       res.status(200).json("Liked Sucessfully");
     } else {
       await comment.updateOne({ $pull: { likes: req.body.userId } });
       res.status(200).json("Unliked Sucessfully");
     }
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
