38589e168714835b80c481f274b7dcb7ef609c2a
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-01-11 23:13:35 +0530

    Add functionality to send account activation email and activate user account

--- routes/auth.js
+++ routes/auth.js
@@ -29,7 +29,7 @@ router.post("/login", async (req, res) => {
     !user && res.status(404).json({ message: "User Not Found!!" });
 
     if (!user.isActivated)
-      res.status(403).json({ message: "Account not activated" });
+      return res.status(403).json({ message: "Account not activated" });
 
     const validPassword = await bcrypt.compare(
       req.body.password,
@@ -57,27 +57,35 @@ router.put("/activate", async (req, res) => {
   try {
     const { token } = req.body;
     if (!token) return res.status(400).send("Missing activation token.");
-    const decoded = jwt.decoded(token);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const currentTime = Math.floor(Date.now() / 1000);
     if (decoded.exp < currentTime) {
-      return res.status(400).json({ message: "Activation link expired" });
+      return res
+        .status(400)
+        .json({
+          message:
+            "Activation link expired, Please request a new activation link",
+        });
     }
-    const user = User.findById(decoded.userId);
-    if(!user){
-      return res.status(404).json({message: "User not found"});
+    const user = await User.findById(decoded.id);
+    console.log(user.id);
+    console.log(decoded.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-    if(user.isActivated){
-      return res.status(400).json({message: "Account already activated"});
+    if (user.isActivated) {
+      return res
+        .status(400)
+        .json({ message: "Account already activated. Please log in" });
     }
-    user.isActivated=true;
+    user.isActivated = true;
     await user.save();
-    res.status(200).json({message:"Account successfully activated"});
+    res.status(200).json({ message: "Account successfully activated" });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
-
-
 
 //if username or email already exists
 router.get("/exists", async (req, res) => {
--- routes/mail.js
+++ routes/mail.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
 let mailTransporter = null;
 
@@ -34,12 +35,50 @@ router.post("/sendMail", async (req, res) => {
   }
 });
 
-router.post("/sendActivationLink", async (req, res) => {
+router.get("/sendActivationLink", async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { email } = req.query;
+
+    const user = await User.findOne({ email: email });
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    const data = await sendActivationMail(user.email, user.username, token);
+
+    return res.status(200).json(data);
   } catch (err) {
     console.log(err);
   }
 });
+
+const sendActivationMail = async (email, username, token) => {
+  try {
+    initMail();
+
+    const mailOptions = {
+      from: `"Social Media App" ${process.env.MAIL_ID}`,
+      to: email,
+      subject: "Account activation link",
+      html: `
+      <html>
+      <body>
+        <div>Hi ${username},</div>
+        <div>Click this <a href="${process.env.FRONTEND_URL}/activate/${token}">link</a> to activate your account</div>
+      </body>
+      </html>
+      `,
+    };
+
+    const res = await mailTransporter.sendMail(mailOptions);
+
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = router;
